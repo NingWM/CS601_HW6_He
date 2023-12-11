e48bdcbed28ae7d5d6c0c343496a7e44d7ff0e11
Glitch (glitch-hello-website) <none>
2023-12-11 13:52:02 -0500

    flipped before & after

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = Vue.createApp({
     data() {
         return {
             pics: [
-                {id:'pic1', before: './images/before1.JPG', after: './images/after1.JPG', current: './images/before1.JPG', cls: 'before'},
-                {id:'pic2', before: './images/before2.JPG', after: './images/after2.JPG', current: './images/before2.JPG', cls: 'before'},
-                {id:'pic3', before: './images/before3.JPG', after: './images/after3.JPG', current: './images/before3.JPG', cls: 'before'},
-                {id:'pic4', before: './images/before4.JPG', after: './images/after4.JPG', current: './images/before4.JPG', cls: 'before'},
-                {id:'pic5', before: './images/before5.JPG', after: './images/after5.JPG', current: './images/before5.JPG', cls: 'before'},
-                {id:'pic6', before: './images/before6.JPG', after: './images/after6.JPG', current: './images/before6.JPG', cls: 'before'}
+                {id:'pic1', before: './images/before1.JPG', after: './images/after1.JPG', current: './images/after1.JPG', cls: 'after'},
+                {id:'pic2', before: './images/before2.JPG', after: './images/after2.JPG', current: './images/after2.JPG', cls: 'after'},
+                {id:'pic3', before: './images/before3.JPG', after: './images/after3.JPG', current: './images/after3.JPG', cls: 'after'},
+                {id:'pic4', before: './images/before4.JPG', after: './images/after4.JPG', current: './images/after4.JPG', cls: 'after'},
+                {id:'pic5', before: './images/before5.JPG', after: './images/after5.JPG', current: './images/after5.JPG', cls: 'after'},
+                {id:'pic6', before: './images/before6.JPG', after: './images/after6.JPG', current: './images/after6.JPG', cls: 'after'}
             ],
             hoverCount: 0
         }
@@ -16,14 +16,14 @@ const app = Vue.createApp({
     methods: {
         mouseover(id) {
             const activPic = this.pics.find(pic => pic.id === id);
-            activPic.current = activPic.after;
-            activPic.cls = 'after';
+            activPic.current = activPic.before;
+            activPic.cls = 'before';
             this.hoverCount++;
         },  
         mouseleave(id){
             const activPic = this.pics.find(pic => pic.id === id);
-            activPic.current = activPic.before;
-            activPic.cls = 'before';
+            activPic.current = activPic.after;
+            activPic.cls = 'after';
         }
     }
 }).mount('#pics');
